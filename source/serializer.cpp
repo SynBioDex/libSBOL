7966201ee0a4e81fe39d94da86eb0fd69a0c1d44
babartley <bartleyba@sbolstandard.org>
2015-02-25 10:40:39 -0800

    C/C++ linkage not working. Multiple definition of 'token' error.  Header files for libxml2 are importing correctly, but attempts to reference a symbol in the library result in the above error.

--- source/serializer.cpp
+++ source/serializer.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include "sbol.h"
-//#include <libxml2/libxml/tree.h>
+#include <libxml/parser.h>
+#include <libxml/xmlstring.h>
+#include <libxml/tree.h>
 //#include <libxml2/libxml/parser.h>
 using namespace std;
 using namespace sbol;
 
 int main() 
 {
-	//xmlDocPtr p = xmlNewDoc(BAD_CAST "1.0");
+	xmlDocPtr p = xmlNewDoc(BAD_CAST "1.0");
 	Identified sbol_obj;
 	sbol_obj.set_identity("http://examples.com", "foo");
 	cout << "Identity:" << sbol_obj.get_identity() << endl;;
