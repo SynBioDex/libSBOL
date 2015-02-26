793c62ac2325279bdfec294c932a9f5e7647656d
babartley <bartleyba@sbolstandard.org>
2015-02-26 12:34:25 -0800

    C/C++ linkage with libxml2 appears to be working.

--- source/serializer.cpp
+++ source/serializer.cpp
@@ -1,16 +1,17 @@
 #include "sbol.h"
 
 #include <iostream>
-
+#include <libxml/parser.h>
+#include <libxml/tree.h>
 //#include <libxml2/libxml/parser.h>
 using namespace std;
 using namespace sbol;
 
 int main() 
 {
-	//xmlDocPtr p = xmlNewDoc(BAD_CAST "1.0");
+	xmlDocPtr p = xmlNewDoc(BAD_CAST "1.0");
 	sbol::Identified sbol_obj = Identified("http://examples.com", "foobar");
-	//sbol_obj.setIdentity("http://examples.com", "foo");
+	sbol_obj.setIdentity("http://examples.com", "foo");
 	cout << "Identity:" << sbol_obj.getIdentity() << endl;;
 	cout << sbol_obj.getTimeStamp() << endl;
 	cerr << "Bye\n";
