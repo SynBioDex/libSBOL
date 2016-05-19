ea02d5d828971e921cbf457aec09ca42649ec7c2
Bryan Bartley <bbartley@u.washington.edu>
2016-05-18 17:04:55 -0700

    Updated gh-pages

--- search/all_1.js
+++ search/all_1.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['component',['Component',['../classsbol_1_1_component.html',1,'sbol']]],
-  ['componentdefinition',['ComponentDefinition',['../classsbol_1_1_component_definition.html',1,'sbol']]],
-  ['componentinstance',['ComponentInstance',['../classsbol_1_1_component_instance.html',1,'sbol']]]
+  ['componentdefinition',['ComponentDefinition',['../classsbol_1_1_component_definition.html',1,'sbol']]]
 ];
--- search/all_c.js
+++ search/all_c.js
@@ -3,7 +3,6 @@ var searchData=
   ['range',['Range',['../classsbol_1_1_range.html',1,'sbol']]],
   ['read',['read',['../classsbol_1_1_document.html#a9143ae31ea81f79a53f18fac75b62843',1,'sbol::Document']]],
   ['referencedobject',['ReferencedObject',['../classsbol_1_1_referenced_object.html',1,'sbol']]],
-  ['referencedobject_3c_20sbol_3a_3acomponentdefinition_20_3e',['ReferencedObject&lt; sbol::ComponentDefinition &gt;',['../classsbol_1_1_referenced_object.html',1,'sbol']]],
   ['referencedobject_3c_20sbol_3a_3ahost_20_3e',['ReferencedObject&lt; sbol::Host &gt;',['../classsbol_1_1_referenced_object.html',1,'sbol']]],
   ['referencedobject_3c_20sbol_3a_3amoduledefinition_20_3e',['ReferencedObject&lt; sbol::ModuleDefinition &gt;',['../classsbol_1_1_referenced_object.html',1,'sbol']]],
   ['roles',['roles',['../classsbol_1_1_component_definition.html#a9e41c934656c96b3561665622902bef3',1,'sbol::ComponentDefinition']]]
