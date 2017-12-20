ae2fbfd5eda230694b8c53f8599c772f84851482
Bryan Bartley <bbartley@u.washington.edu>
2017-12-19 16:02:54 -0800

    C++ tester application now accepts command line arguments

--- test/test.cpp
+++ test/test.cpp
@@ -14,42 +14,61 @@
 using namespace std;
 using namespace sbol;
 
-int main()
+
+bool roundtrip(string path, string filename)
 {
-    int passed = 0;
-    int failed = 0;
-    
     Document& doc = *new Document();
     Document& doc2 = *new Document();
 
-    string path = "roundtrip";
-    DIR* valid = opendir(path.c_str());
-    struct dirent * file = readdir(valid);
-    while (file)
+    std::cout << "Testing " << filename << std::endl;
+    doc.read(path + "/" + filename);
+    std::cout << "Writing " << filename << std::endl;
+    doc.write(path + "/" + "new_" + filename);
+    std::cout << "Reading " << filename << std::endl;
+    doc2.read(path + "/" + "new_" + filename);
+    int is_equal_to = doc.compare(&doc2);
+    if (is_equal_to)
     {
-        if (file->d_name[0] != '.')
-        {
-            string filename = string(file->d_name);
-            std::cout << "Testing " << filename << std::endl;
-            doc.read(path + "/" + filename);
-            std::cout << "Writing " << filename << std::endl;
-            doc.write(path + "/" + "new_" + filename);
-            std::cout << "Reading " << filename << std::endl;
-            doc2.read(path + "/" + "new_" + filename);
-            int is_equal_to = doc.compare(&doc2);
-            if (is_equal_to)
-            {
-                std::cout << "=== " << filename << " passed ===" << std::endl;
-                ++passed;
-            }
-            else
-            {
-                std::cout << "XXX " << filename << " failed XXX" << std::endl;
-                ++failed;
-            }
-        }
-        file = readdir(valid);
+        std::cout << "=== " << filename << " passed ===" << std::endl;
+        return true;
     }
+    else
+    {
+        std::cout << "XXX " << filename << " failed XXX" << std::endl;
+        return false;
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    string path = "roundtrip";
+    int passed = 0;
+    int failed = 0;
+    
+    if (argc > 1)
+    {
+        if (roundtrip(path, string(argv[1])))
+            passed++;
+        else
+            failed++;
+    }
+    else
+    {
+        DIR* valid = opendir(path.c_str());
+        struct dirent * file = readdir(valid);
+        while (file)
+        {
+            if (file->d_name[0] != '.')
+            {
+                if (roundtrip(path, file->d_name))
+                    passed++;
+                else
+                    failed++;
+            }
+            file = readdir(valid);
+        }
+    }
+
     cout << "Passed: " << passed << endl;
     cout << "FAILED: " << failed << endl;
     
