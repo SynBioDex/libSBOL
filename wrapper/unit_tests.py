8f71f2494ec53c6787647298a56091e507a8a297
Bryan Bartley <bryan.a.bartley@raytheon.com>
2018-07-18 16:41:10 -0400

    Add more tests

--- wrapper/unit_tests.py
+++ wrapper/unit_tests.py
@@ -582,6 +582,17 @@ class TestIterators(unittest.TestCase):
 	def setUp(self):
 		pass
 	
+	def testDocumentIterator(self):
+		doc = Document()
+		cds = []
+		for i_cd in range(0, 10):
+			cd = doc.componentDefinitions.create('cd%d' %i_cd)
+			cds.append(cd.identity)
+		i_cd = 0
+		for obj in doc:
+			cds.remove(obj.identity)
+		self.assertEquals([], cds)
+
 	def testOwnedObjectIterator(self):
 		cd = ComponentDefinition()
 		sa1 = cd.sequenceAnnotations.create('sa1').this
@@ -648,7 +659,60 @@ class TestCopy(unittest.TestCase):
 		tl = doc2.getExtensionObject(tl.identity)
 		self.assertEquals(tl.thisown, False)
 
-def runTests(test_list = [TestComponentDefinitions, TestSequences, TestMemory, TestIterators, TestCopy ]):
+class TestDBTL(unittest.TestCase):
+	def testDBTL(self):	
+		setHomespace("http://examples.org")
+
+		doc = Document()
+		workflow_step_1 = Activity('build_1')
+		workflow_step_2 = Activity('build_2')
+		workflow_step_3 = Activity('build_3')
+		workflow_step_4 = Activity('build_4')
+		workflow_step_5 = Activity('build_5')
+		workflow_step_6 = Activity('test_1')
+		workflow_step_7 = Activity('analysis_1')
+
+		workflow_step_1.plan = Plan('PCR_protocol_part1')
+		workflow_step_2.plan = Plan('PCR_protocol_part2')
+		workflow_step_3.plan = Plan('PCR_protocol_part3')
+		workflow_step_4.plan = Plan('gibson_assembly')
+		workflow_step_5.plan = Plan('transformation')
+		workflow_step_6.plan = Plan('promoter_characterization')
+		workflow_step_7.plan = Plan('parameter_optimization')
+
+		setHomespace('')
+		Config.setOption('sbol_compliant_uris', False)  # Temporarily disable auto-construction of URIs
+
+		workflow_step_1.agent = Agent('mailto:jdoe@sbols.org')
+		workflow_step_2.agent = workflow_step_1.agent
+		workflow_step_3.agent = workflow_step_1.agent
+		workflow_step_4.agent = workflow_step_1.agent
+		workflow_step_5.agent = workflow_step_1.agent
+		workflow_step_6.agent = Agent('http://sys-bio.org/plate_reader_1')
+		workflow_step_7.agent = Agent('http://tellurium.analogmachine.org')
+
+		Config.setOption('sbol_compliant_uris', True)
+		setHomespace("http://examples.org")
+
+		doc.addActivity([workflow_step_1, workflow_step_2, workflow_step_3, workflow_step_4, workflow_step_5, workflow_step_6, workflow_step_7])
+
+		doc.componentDefinitions.create('cd')
+		target = Design('target')
+		part1 = workflow_step_1.generateBuild('part1', target)
+		part2 = workflow_step_2.generateBuild('part2', target)
+		part3 = workflow_step_3.generateBuild('part3', target)
+		gibson_mix = workflow_step_4.generateBuild('gibson_mix', target, [part1, part2, part3])
+		clones = workflow_step_5.generateBuild(['clone1', 'clone2', 'clone3'], target, gibson_mix)
+		experiment1 = workflow_step_6.generateTest('experiment1', clones)
+		analysis1 = workflow_step_7.generateAnalysis('analysis1', experiment1)
+		doc.readString(doc.writeString())
+
+		activity = doc.activities['build_1']
+		self.assertEquals(activity.agent.identity, activity.associations[0].agent)
+		self.assertEquals(activity.plan.identity, activity.associations[0].plan)
+
+
+def runTests(test_list = [TestComponentDefinitions, TestSequences, TestMemory, TestIterators, TestCopy, TestDBTL ]):
 	print("Setting up")
 	#exec(open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "CRISPR_example.py")).read())
 	suite_list = []
