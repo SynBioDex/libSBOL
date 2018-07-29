978c2de3375865c08e2469987cb77674aaceaa26
Bryan Bartley <bryan.a.bartley@raytheon.com>
2018-07-29 01:20:06 -0400

    Prepare pySBOL post19

--- wrapper/__init__.py
+++ wrapper/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
 __version__ = '2.3.0'
-__commit__ = 'e2a2977660ade83ca32db644a4a28872befffd10'
+__commit__ = 'ed3518f7e3a71933ab2ec051814a1fed34c7a704'
 
 from sbol.sbol import *
 import sbol.unit_tests
